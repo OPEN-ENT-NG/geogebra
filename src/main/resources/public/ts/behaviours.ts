c5a8107b6897a1cdc6f7e4b14399544b84143c1d
Samuel JOLLOIS <sjollois@ensc.fr>
2022-09-19 18:12:00 +0200

    fix return PR

--- src/main/resources/public/ts/behaviours.ts
+++ src/main/resources/public/ts/behaviours.ts
@@ -1,5 +1,13 @@
 import { Behaviours } from 'entcore';
 
+export const GEOGEBRA_APP = "geogebra";
+
+export const GEOGEBRA_EXTENSION = ".ggb";
+
+export const GEOGEBRA_METADATA_TYPE = "application/octet-stream";
+
+export const GEOGEBRA_FILENAME_URL = "?fileName=";
+
 Behaviours.register('geogebra', {
     rights: {
         workflow: {
--- src/main/resources/public/ts/controllers/main.ts
+++ src/main/resources/public/ts/controllers/main.ts
@@ -1,6 +1,7 @@
 import {Document, navigationGuardService, ng, template, toasts, workspace} from 'entcore';
 import {Utils} from "../utils/Utils";
 import http, {AxiosResponse} from "axios";
+import {GEOGEBRA_APP, GEOGEBRA_EXTENSION, GEOGEBRA_FILENAME_URL, GEOGEBRA_METADATA_TYPE} from "../behaviours";
 
 
 declare var GGBApplet: any;
@@ -55,8 +56,10 @@ export const mainController = ng.controller('MainController', ['$timeout','$scop
                 clearInterval(int);
                 const hash : string = window.location.hash;
                 if(hash.length > 0){
-                    window.documentId = hash.substring(hash.indexOf("#/") + 2, hash.indexOf("?fileName="));
-                    window.fileName = hash.substring(hash.indexOf("?fileName=") + 10, hash.indexOf(".ggb") + 4)
+                    window.documentId = hash.substring(hash.indexOf("#/") + 2, hash.indexOf(GEOGEBRA_FILENAME_URL));
+                    window.fileName = hash.substring(
+                        hash.indexOf(GEOGEBRA_FILENAME_URL) + GEOGEBRA_FILENAME_URL.length,
+                        hash.indexOf(GEOGEBRA_EXTENSION) + GEOGEBRA_EXTENSION.length)
                 }
                 if (!!window.documentId) {
                     $scope.data.documentId = window.documentId;
@@ -92,7 +95,7 @@ export const mainController = ng.controller('MainController', ['$timeout','$scop
 
     $scope.getGGB = async () : Promise<void> => {
         if ($scope.data.documentSelected && $scope.data.documentSelected.metadata && $scope.data.documentSelected.metadata.extension &&
-            $scope.data.documentSelected.metadata.extension == "ggb") {
+            GEOGEBRA_EXTENSION.endsWith($scope.data.documentSelected.metadata.extension)) {
             try {
                 const {id} = extractNameAndIdProject();
                 await setGGBAplet(id);
@@ -112,10 +115,7 @@ export const mainController = ng.controller('MainController', ['$timeout','$scop
         $scope.data.base64 = file.data.base64File;
         const {fileName} = extractNameAndIdProject();
         $scope.data.fileName = fileName;
-        await Utils.safeApply($scope);
-        navigationGuardService.reset("geogebra");
-        window.location.hash = `${id}?fileName=${fileName}`;
-
+        await resetAndSetHash(id, fileName);
     }
 
     $scope.getGGBById = async () : Promise<void> => {
@@ -127,21 +127,25 @@ export const mainController = ng.controller('MainController', ['$timeout','$scop
         }
     }
 
+    async function resetAndSetHash(id: string, fileName: string) : Promise<void> {
+        navigationGuardService.reset(GEOGEBRA_APP);
+        window.location.hash = `${id}${GEOGEBRA_FILENAME_URL}${fileName}`;
+        await Utils.safeApply($scope);
+    }
+
     $scope.saveDocument = (name: string) : void => {
         try {
             const u8arr : Uint8Array = extractedFileBinary();
-            let file : File = new File([u8arr], name + ".ggb", {type: 'application/octet-stream'});
+            let file : File = new File([u8arr], name + GEOGEBRA_EXTENSION, {type: GEOGEBRA_METADATA_TYPE});
             let doc : Document = new Document();
             workspace.v2.service.createDocument(file, doc, null,
                 {visibility: "protected", application: "media-library"}).then(async data => {
                 $scope.data.documentSelected = data;
-                http.post("geogebra");
-                toasts.confirm("Sauvegarde effectuée dans \"Documents ajoutés dans les applis\"");
+                http.post(GEOGEBRA_APP);
+                toasts.confirm("geogebra.save.success.applis");
                 $scope.displayState.name = false;
-                navigationGuardService.reset("geogebra");
                 const {fileName, id} = extractNameAndIdProject();
-                window.location.hash = `${id}?fileName=${fileName}`;
-                await Utils.safeApply($scope);
+                await resetAndSetHash(id, fileName);
             });
         } catch (e) {
             toasts.warning(e.error);
@@ -163,7 +167,7 @@ export const mainController = ng.controller('MainController', ['$timeout','$scop
 
     function extractNameAndIdProject() : { fileName: string, id: string } {
         let fileName : string = ($scope.data.documentSelected) ? $scope.data.documentSelected.metadata.filename : $scope.data.fileName;
-        if (!fileName.endsWith(".ggb")) fileName += ".ggb";
+        if (!fileName.endsWith(GEOGEBRA_EXTENSION)) fileName += GEOGEBRA_EXTENSION;
         const id : string = ($scope.data.documentSelected) ?
             (($scope.data.documentSelected.data) ? $scope.data.documentSelected.data._id : $scope.data.documentSelected._id)
             : $scope.data.documentId;
@@ -174,14 +178,14 @@ export const mainController = ng.controller('MainController', ['$timeout','$scop
         try {
             const u8arr : Uint8Array = extractedFileBinary();
             const {fileName, id} = extractNameAndIdProject();
-            let file : File = new File([u8arr], fileName, {type: 'application/octet-stream'});
+            let file : File = new File([u8arr], fileName, {type: GEOGEBRA_METADATA_TYPE});
             let doc : Document = new Document();
             doc.name = fileName;
             doc._id = id;
             await workspace.v2.service.updateDocument(file, doc);
-            http.post("geogebra");
-            toasts.confirm("Sauvegarde effectuée");
-            navigationGuardService.reset("geogebra");
+            http.post(GEOGEBRA_APP);
+            toasts.confirm("geogebra.save.success");
+            navigationGuardService.reset(GEOGEBRA_APP);
         } catch (e) {
             toasts.warning(e.error);
             throw (e);
