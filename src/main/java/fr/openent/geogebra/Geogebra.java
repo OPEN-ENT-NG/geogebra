89bd1aed0c66418e85908a7c66b6832cfb277766
alex.delaveau <alex.delaveau@cgi.com>
2024-12-20 13:47:55 +0100

    feat(migration): #MEX-418 migration vertx

--- src/main/java/fr/openent/geogebra/Geogebra.java
+++ src/main/java/fr/openent/geogebra/Geogebra.java
@@ -2,14 +2,16 @@ package fr.openent.geogebra;
 
 import fr.openent.geogebra.controllers.GeogebraController;
 import org.entcore.common.http.BaseServer;
+import io.vertx.core.Promise;
 
 public class Geogebra extends BaseServer {
 
 	@Override
-	public void start() throws Exception {
-		super.start();
-
+	public void start(Promise<Void> startPromise) throws Exception {
+		super.start(startPromise);
 		addController(new GeogebraController());
+		startPromise.tryComplete();
+		startPromise.tryFail("[Geogebra@Geogebra::start] Fail to start Geogebra");
 	}
 
 }
