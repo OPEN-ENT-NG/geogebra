fd4b3af31f1d582521e72a0f187ca9681b1c2059
Junior Bernard <junior.bernard@opendigitaleducation.com>
2025-12-11 15:49:42 +0100

    feat: #RBACK-117, kubernetes migration

--- src/main/java/fr/openent/geogebra/Geogebra.java
+++ src/main/java/fr/openent/geogebra/Geogebra.java
@@ -1,17 +1,24 @@
 package fr.openent.geogebra;
 
 import fr.openent.geogebra.controllers.GeogebraController;
+import io.vertx.core.Future;
 import org.entcore.common.http.BaseServer;
 import io.vertx.core.Promise;
+
+import static io.vertx.core.Future.succeededFuture;
 
 public class Geogebra extends BaseServer {
 
 	@Override
 	public void start(Promise<Void> startPromise) throws Exception {
-		super.start(startPromise);
+		final Promise<Void> promise = Promise.promise();
+		super.start(promise);
+		promise.future().compose(e -> initGeogebra()).onComplete(startPromise)
+				.onFailure(th -> log.error("[Geogebra@Geogebra::start] Fail to start Geogebra", th));
+	}
+	public Future<Void> initGeogebra() {
 		addController(new GeogebraController());
-		startPromise.tryComplete();
-		startPromise.tryFail("[Geogebra@Geogebra::start] Fail to start Geogebra");
+		return succeededFuture();
 	}
 
 }
